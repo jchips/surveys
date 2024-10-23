74668b8f933c61d84f911e4af12a5d442021607d
Jchips <20979096+Jchips@users.noreply.github.com>
2024-10-22 20:29:15 -0700

    trying to fix error

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
+import { useEffect } from 'react';
 import { StyleSheet, Text } from 'react-native';
 import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 import { AuthProvider } from './src/contexts/AuthContext';
 import Routing from './src/routing/Routing';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   Text.defaultProps = Text.defaultProps || {};
   Text.defaultProps.allowFontScaling = false; // Disable font scaling
   Text.defaultProps.textBreakStrategy = 'simple'; // Adjust text break strategy
-  const [fontLoaded] = useFonts({
+  const [fontsLoaded, error] = useFonts({
     'Inter-Regular': require('./assets/fonts/Inter-Regular.ttf'),
     'Inter-Bold': require('./assets/fonts/Inter-Bold.ttf'),
     'Inter-SemiBold': require('./assets/fonts/Inter-SemiBold.ttf'),
     'Inter-ExtraBold': require('./assets/fonts/Inter-ExtraBold.ttf'),
     'Inter-BoldItalic': require('./assets/fonts/Inter-BoldItalic.ttf'),
   });
-  return fontLoaded ? (
+
+  useEffect(() => {
+    if (fontsLoaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
+  return fontsLoaded ? (
     <AuthProvider>
       <Routing />
     </AuthProvider>
